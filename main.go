78ff7519d9975bac4289b5ce50deabdcaae50869
Paulo Junior <juniorestudante@hotmail.com>
2020-07-14 18:12:52 -0300

    [FIX] adicionando produtos por struct e pegando dinamicamente

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
+type Medicao struct {
+	DataHora        string
+	Batimentos      int
+	NivelOxigenacao int
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	fmt.Println("Servidor rodando na porta 321")
@@ -15,6 +21,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	//temp.ExecuteTemplate(w, "index.html", nil)
-	temp.ExecuteTemplate(w, "Index", nil)
+	medicoes := []Medicao{
+		{DataHora: "14/07/2020 18:06",
+			Batimentos:      69,
+			NivelOxigenacao: 97},
+		{"14/07/2020 18:08", 69, 98},
+	}
+	temp.ExecuteTemplate(w, "Index", medicoes)
 }
