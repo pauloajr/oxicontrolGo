4b6a68bc2f84bd00c24a3dd06af07a9f32020596
Paulo Junior <juniorestudante@hotmail.com>
2020-07-14 23:58:03 -0300

    [FIX] adicionando conexao ao bd com selecao e vendor

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 
@@ -22,25 +23,55 @@ func ConectarBD() *sql.DB {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 type Medicao struct {
+	Id              int
 	DataHora        string
 	Batimentos      int
 	NivelOxigenacao int
 }
 
 func main() {
-	db := ConectarBD()
-	defer db.Close()
 	http.HandleFunc("/", index)
 	fmt.Println("Servidor rodando na porta 321")
 	http.ListenAndServe(":321", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	medicoes := []Medicao{
+	db := ConectarBD()
+	defer db.Close()
+	/*medicoes := []Medicao{
 		{DataHora: "14/07/2020 18:06",
 			Batimentos:      69,
 			NivelOxigenacao: 97},
 		{"14/07/2020 18:08", 69, 98},
+	}*/
+
+	selecionarMedicoes, err := db.Query("SELECT * FROM medicoes")
+	if err != nil {
+		log.Fatal(err.Error())
 	}
+
+	m := Medicao{}
+	medicoes := []Medicao{}
+
+	for selecionarMedicoes.Next() {
+		var id int
+		var batimentos, nivelOxigenacao int
+		var dataHora string
+
+		err = selecionarMedicoes.Scan(&id, &dataHora, &batimentos, &nivelOxigenacao)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		m.Id = id
+		m.DataHora = dataHora
+		m.Batimentos = batimentos
+		m.NivelOxigenacao = nivelOxigenacao
+
+		medicoes = append(medicoes, m)
+	}
+
 	temp.ExecuteTemplate(w, "Index", medicoes)
+
+	defer db.Close()
 }
