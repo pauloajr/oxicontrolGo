e1118dfdc2b6477f113de4648fa37b74c2e06097
Paulo Junior <juniorestudante@hotmail.com>
2020-07-14 23:45:13 -0300

    [FEAT] Parte 2 - banco de dados

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
+
+func ConectarBD() *sql.DB {
+	conexao := "user=postgres dbname=oxidb password=yourpassword host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+
+	if err != nil {
+		panic("Nao conseguiu conectar com o banco de dados." + err.Error())
+	}
+	return db
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -15,6 +28,8 @@ type Medicao struct {
 }
 
 func main() {
+	db := ConectarBD()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	fmt.Println("Servidor rodando na porta 321")
 	http.ListenAndServe(":321", nil)
