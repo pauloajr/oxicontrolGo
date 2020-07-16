3fc736b93564f4a03c0026bcf2d60dabefb5900e
Paulo Junior <juniorestudante@hotmail.com>
2020-07-16 01:05:04 -0300

    [FEAT] Parte 3 - Crud Completo

--- controllers/medicoes.go
+++ controllers/medicoes.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
+	"time"
 
 	"../model"
 )
@@ -13,4 +16,69 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	medicoes := model.BuscaTodasMedicao()
 	temp.ExecuteTemplate(w, "Index", medicoes)
 
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		bati := r.FormValue("bati-min")
+		sp02 := r.FormValue("sp02")
+
+		batiConvertInt, err := strconv.Atoi(bati)
+		if err != nil {
+			log.Println("bati nao conseguiu converter", err.Error())
+		}
+		sp02ConvertInt, err2 := strconv.Atoi(sp02)
+		if err2 != nil {
+			log.Println("sp02 nao conseguiu converter", err.Error())
+		}
+
+		data := time.Now()
+
+		dataFinal := string(data.Format(("2006-01-02 15:04:05 ")))
+
+		model.CriarNovaMedicao(dataFinal, batiConvertInt, sp02ConvertInt)
+	}
+	http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	idMedicao := r.URL.Query().Get("id")
+	model.DeletarMedicao(idMedicao)
+	http.Redirect(w, r, "/", 301)
+
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		bati := r.FormValue("bati-min")
+		sp02 := r.FormValue("sp02")
+		id := r.FormValue("id")
+		dthr := r.FormValue("dthr")
+
+		batiConvertInt, err := strconv.Atoi(bati)
+		if err != nil {
+			log.Println("bati nao conseguiu converter", err.Error())
+		}
+		sp02ConvertInt, err2 := strconv.Atoi(sp02)
+		if err2 != nil {
+			log.Println("sp02 nao conseguiu converter", err.Error())
+		}
+		idConvertInt, err3 := strconv.Atoi(id)
+		if err3 != nil {
+			log.Println("id nao conseguiu converter", err.Error())
+		}
+
+		model.AtualizaProduto(idConvertInt, batiConvertInt, sp02ConvertInt, dthr)
+	}
+	http.Redirect(w, r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idMedicao := r.URL.Query().Get("id")
+	produto := model.BuscarProduto(idMedicao)
+	temp.ExecuteTemplate(w, "Edit", produto)
 }
--- model/medicoes.go
+++ model/medicoes.go
@@ -45,3 +45,72 @@ func BuscaTodasMedicao() []Medicao {
 
 	return medicoes
 }
+
+func CriarNovaMedicao(dataHora string, bati, sp02 int) {
+	db := bd.ConectarBD()
+
+	insertDadosNoBanco, err := db.Prepare("INSERT INTO medicoes (datahora, batimentos, niveloxigenacao) VALUES ($1,$2,$3)")
+	if err != nil {
+		log.Println("Houve um erro ao inserir o dado. " + err.Error())
+	}
+
+	insertDadosNoBanco.Exec(dataHora, bati, sp02)
+	defer db.Close()
+}
+
+func DeletarMedicao(idMedicao string) {
+	db := bd.ConectarBD()
+
+	deletarDados, err := db.Prepare("DELETE FROM medicoes WHERE id = $1")
+	if err != nil {
+		log.Println("Houve um erro ao deletar o dado. " + err.Error())
+	}
+	deletarDados.Exec(idMedicao)
+
+	defer db.Close()
+}
+
+func BuscarProduto(idMedicao string) Medicao {
+	db := bd.ConectarBD()
+
+	selecionarMedicoes, err := db.Query("SELECT * FROM medicoes WHERE id = $1", idMedicao)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	medicaoSelecionada := Medicao{}
+
+	for selecionarMedicoes.Next() {
+		var id int
+		var batimentos, nivelOxigenacao int
+		var dataHora string
+
+		err = selecionarMedicoes.Scan(&id, &dataHora, &batimentos, &nivelOxigenacao)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		medicaoSelecionada.Id = id
+		medicaoSelecionada.DataHora = dataHora
+		medicaoSelecionada.Batimentos = batimentos
+		medicaoSelecionada.NivelOxigenacao = nivelOxigenacao
+
+	}
+
+	defer db.Close()
+
+	return medicaoSelecionada
+}
+
+func AtualizaProduto(idConvertInt, batiConvertInt, sp02ConvertInt int, dthr string) {
+	db := bd.ConectarBD()
+
+	AtualizarProduto, err := db.Prepare("UPDATE medicoes SET datahora=$1, batimentos=$2, niveloxigenacao=$3 WHERE id = $4")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	AtualizarProduto.Exec(dthr, batiConvertInt, sp02ConvertInt, idConvertInt)
+
+	defer db.Close()
+}
